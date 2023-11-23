40b2d6f160d5c26c6417ed507d8ba6ba4ab8d0a5
Nils Kimenai <493217@student.saxion.nl>
2023-11-23 10:15:15 +0100

    linting configuration and correction

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,8 +1,21 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
+const importOrderRule = ['error', {
+  'newlines-between': 'always',
+  groups: [
+    ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+    ['type'],
+  ],
+}];
+
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
@@ -11,5 +24,25 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  plugins: [
+    'import', 'import-newlines'
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'new-cap': 'off',
+    'object-curly-spacing': ['error', 'always'],
+    'max-len': ['error', {
+      code: 120,
+    }],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+    ],
+    'no-multi-spaces': ['error'],
+    'import-newlines/enforce': ['error',
+      { items: 3 },
+    ],
+    'import/order': importOrderRule,
+  },
 }
