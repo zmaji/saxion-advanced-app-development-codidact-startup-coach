785f16eb7a91e316f8865b38b03ec3765278ffd1
Nils Kimenai <493217@student.saxion.nl>
2023-11-24 10:24:53 +0100

    Frontend fonts for styling and use in app

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,7 @@
-import './assets/main.css'
+// import './assets/main.css'
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.css'
+import './assets/scss/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
