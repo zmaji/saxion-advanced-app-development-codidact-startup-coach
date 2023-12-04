2339fb17eb743a0dd75a89d189a9588f1ce2f44f
Nils Kimenai <493217@student.saxion.nl>
2023-12-04 14:40:51 +0100

    Component overview page and base pages for routing + linting

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,8 +5,8 @@ import './assets/scss/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './App.vue'
 import router from './router'
+import App from './App.vue'
 
 const app = createApp(App)
 
