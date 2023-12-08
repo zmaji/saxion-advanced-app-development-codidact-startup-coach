3050f8722c73011352ec27606ae8f6c94641d34c
Saied Omar <470713@student.saxion.nl>
2023-12-08 17:09:53 +0100

    Toegangsniveau dropdown toegevoegd

--- frontend/src/views/content/Create.vue
+++ frontend/src/views/content/Create.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+  import { Ref, ref } from 'vue';
+
   import { PageTitle, SecondaryTitle, TextButton } from '@/components'
   import { LabelSelect } from '@/components'
-  
+
+
+  let accessLevel: Ref<string> = ref('');
+
 </script>
 
 <template>
@@ -14,45 +19,35 @@
       <SecondaryTitle> Algemene gegevens </SecondaryTitle>
 
       <form>
-          <div class="pb-3 col-lg-10">
-            <label for="title" class="form-label">Titel</label>
+        <div class="pb-3 col-lg-10">
+          <label for="title" class="form-label">Titel</label>
 
-            <input
-              type="text"
-              class="form-control"
-              id="title"
-              placeholder="Bijvoorbeeld: Mijn Leuke Titel"
-            />
-          </div>
+          <input type="text" class="form-control" id="title" placeholder="Bijvoorbeeld: Mijn Leuke Titel" />
+        </div>
 
-          <div class="pb-3 col-lg-10">
-            <label for="description" class="form-label">Beschrijving</label>
+        <div class="pb-3 col-lg-10">
+          <label for="description" class="form-label">Beschrijving</label>
 
-            <textarea
-              type="text"
-              class="form-control"
-              id="description"
-              placeholder="Bijvoorbeeld: Mijn Content is handig !"
-              rows="5"
-              style="resize: none"
-            />
-          </div>
+          <textarea type="text" class="form-control" id="description"
+           placeholder="Bijvoorbeeld: Mijn Content is handig !"
+            rows="5" style="resize: none" />
+        </div>
 
-          <div class="pb-3 col-lg-10">
-            <label class="pb-2" for="category">Categorie</label>
-            <select class="form-select" id="category">
-              <option selected>Selecteer een categorie</option>
-              <option value="1">Software</option>
-              <option value="2">Zorg</option>
-              <option value="3">Business</option>
-            </select>
-          </div>
+        <div class="pb-3 col-lg-10">
+          <label class="pb-2" for="category">Categorie</label>
+          <select class="form-select" id="category">
+            <option selected>Selecteer een categorie</option>
+            <option value="1">Software</option>
+            <option value="2">Zorg</option>
+            <option value="3">Business</option>
+          </select>
+        </div>
 
-          <div class="pb-3 col-lg-10">
-            <label for="formFile" class="form-label">Bijlage</label>
+        <div class="pb-3 col-lg-10">
+          <label for="formFile" class="form-label">Bijlage</label>
 
-            <input class="form-control" type="file" id="formFile" />
-          </div>
+          <input class="form-control" type="file" id="formFile" />
+        </div>
       </form>
     </div>
 
@@ -60,11 +55,24 @@
       <SecondaryTitle> Content labels </SecondaryTitle>
 
       <label class="form-label">Content labels</label>
-      <LabelSelect/>
+      <LabelSelect />
     </div>
 
     <div class="col">
       <SecondaryTitle> Toegangsniveau </SecondaryTitle>
+      <div class="pb-3 col-lg-10">
+        <label class="pb-2" for="category">Zichtbaarheid</label>
+        <select class="form-select" id="category" v-model="accessLevel">
+          <option selected value="">Selecteer toegangsniveau (Standaard Openbaar)</option>
+          <option value="private">Privé</option>
+          <option value="restricted">Beperkt</option>
+          <option value="public">Openbaar</option>
+        </select>
+      </div>
+
+      <div v-if="accessLevel === 'restricted'">
+          
+      </div>
 
       <div class="d-flex flex-md-row flex-column flex-wrap">
         <TextButton class="mb-3 mb-md-0 me-md-2"> Toevoegen </TextButton>
