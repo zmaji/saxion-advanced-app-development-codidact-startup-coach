038e0dfa803fbfcf1b710d8ffa8b45b968db0c11
Nils Kimenai <493217@student.saxion.nl>
2023-12-08 14:31:48 +0100

    typo fix

--- frontend/src/components/buttons/IconButton.vue
+++ frontend/src/components/buttons/IconButton.vue
@@ -27,7 +27,7 @@
   });
 
   const slots = useSlots();
-  const hasDefualtSlot = !!slots['default'];
+  const hasDefaultSlot = !!slots['default'];
 
   const dynamicDomClasses = computed<string>(() => {
     switch (props.displayStyle) {
@@ -36,20 +36,20 @@
         btn text-${(props.type ? props.type : 'primary')}
         bg-${(props.type ? props.type : 'primary')}-subtle
         ${props.size ? 'btn-' + props.size : ''}
-        ${props.renderAsCircle && !hasDefualtSlot ? 'btn-circle' : 'px-4 py-2'}
+        ${props.renderAsCircle && !hasDefaultSlot ? 'btn-circle' : 'px-4 py-2'}
        `;
     case 'tertiary':
       return `
         btn bg-white text-${(props.type ? props.type : 'primary')}
         ${props.size ? 'btn-' + props.size : ''}
-        ${props.renderAsCircle && !hasDefualtSlot ? 'btn-circle' : 'px-4 py-2'}
+        ${props.renderAsCircle && !hasDefaultSlot ? 'btn-circle' : 'px-4 py-2'}
        `;
     default:
       // primary
       return `
         btn btn-${(props.type ? props.type : 'primary')}
         ${props.size ? 'btn-' + props.size : ''}
-        ${props.renderAsCircle && !hasDefualtSlot ? 'btn-circle' : 'px-4 py-2'}
+        ${props.renderAsCircle && !hasDefaultSlot ? 'btn-circle' : 'px-4 py-2'}
        `;
     }
   });
@@ -61,7 +61,7 @@
     :type="buttonType ? buttonType : 'submit'"
     :class="dynamicDomClasses"
   >
-    <font-awesome-icon :icon="icon" :class="hasDefualtSlot ? 'me-2' : null"/>
+    <font-awesome-icon :icon="icon" :class="hasDefaultSlot ? 'me-2' : null"/>
 
     <slot></slot>
   </button>
@@ -72,7 +72,7 @@
     class=""
     :to="to"
   >
-    <font-awesome-icon :icon="icon" :class="hasDefualtSlot ? 'me-1' : null"/>
+    <font-awesome-icon :icon="icon" :class="hasDefaultSlot ? 'me-1' : null"/>
 
     <slot></slot>
   </RouterLink>
