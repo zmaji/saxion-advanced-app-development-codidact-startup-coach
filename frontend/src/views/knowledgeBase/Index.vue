4e61fe90366859fab9b222869de40b931ffa67c9
Nils Kimenai <493217@student.saxion.nl>
2023-12-11 12:03:19 +0100

    fetch categories and render in tree

--- frontend/src/views/knowledgeBase/Index.vue
+++ frontend/src/views/knowledgeBase/Index.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 
   import type { Category } from '@/typings/Content';
+  import type { Ref } from 'vue';
+
+  import { onMounted, ref } from 'vue';
 
   import {
     ContentCategory,
@@ -8,43 +11,63 @@
     SubTitle,
     SecondaryTitle
   } from '@/components';
+  import httpService from '@/plugins/http/httpService';
 
-  const categories: Category[] = [
-    {
-      categoryID: '1',
-      name: 'Main Category 1',
-      subCategories: [
-        {
-          categoryID: '1-1',
-          parentCategory: '1',
-          name: 'Subcategory 1-1',
-          subCategories: [
-            {
-              categoryID: '1-1-1',
-              parentCategory: '1-1',
-              name: 'Sub-subcategory 1-1-1'
-            },
-            {
-              categoryID: '1-1-2',
-              parentCategory: '1-1',
-              name: 'Sub-subcategory 1-1-2'
-            }
-          ]
-        },
-        {
-          categoryID: '1-2',
-          parentCategory: '1',
-          name: 'Subcategory 1-2'
-        }
-      ]
-    },
-    {
-      categoryID: '2',
-      name: 'Main Category 2',
-      subCategories: []
+  // const categories: Category[] = [
+  //   {
+  //     categoryID: '1',
+  //     name: 'Main Category 1',
+  //     subCategories: [
+  //       {
+  //         categoryID: '1-1',
+  //         parentCategory: '1',
+  //         name: 'Subcategory 1-1',
+  //         subCategories: [
+  //           {
+  //             categoryID: '1-1-1',
+  //             parentCategory: '1-1',
+  //             name: 'Sub-subcategory 1-1-1'
+  //           },
+  //           {
+  //             categoryID: '1-1-2',
+  //             parentCategory: '1-1',
+  //             name: 'Sub-subcategory 1-1-2'
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         categoryID: '1-2',
+  //         parentCategory: '1',
+  //         name: 'Subcategory 1-2'
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     categoryID: '2',
+  //     name: 'Main Category 2',
+  //     subCategories: []
+  //   }
+  // ];
+
+  const categories: Ref<Category[]> = ref<Category[]>([]);
+
+  const fetchCategories = async () => {
+
+    try {
+      const response = await httpService.getRequest<Category[]>('/categories', false);
+      console.log(response.data)
+
+      if (response && response.data) {
+        categories.value = response.data;
+      }
+    } catch (e) {
+      console.log(e);
     }
-  ];
+  }
 
+  onMounted(() => {
+    fetchCategories();
+  });
 </script>
 
 <template>
