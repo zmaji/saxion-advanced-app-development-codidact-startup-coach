6184fc361811e2ecb6c660771992c447f31ef802
Saied Omar <470713@student.saxion.nl>
2023-12-11 17:02:34 +0100

    comit

--- frontend/src/typings/content.d.ts
+++ frontend/src/typings/content.d.ts
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
 export interface Category {
   categoryID: string,
   parentCategory?: string,
   name: string,
   subCategories?: Category[]
 }
->>>>>>> 3f9152ef45769f230aee27b4dd5a20622093ac90
