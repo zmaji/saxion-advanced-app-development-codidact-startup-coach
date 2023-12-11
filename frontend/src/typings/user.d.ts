0b44fa313620a725923e0d02ec420b9bc064c966
Zikria Azimi <zikria.azimi@topicus.nl>
2023-12-11 16:16:58 +0100

    changes

--- frontend/src/typings/user.d.ts
+++ frontend/src/typings/user.d.ts
@@ -1,4 +1,11 @@
-export interface ContentUser {
-    userID: string;
-    name: string;
+import type { Company } from './Module';
+
+export interface User {
+  userID: string,
+  company?: Company | string,
+  userName: string,
+  password: string,
+  secret: string,
+  emailAddress: string,
+  fullName: string
 }
