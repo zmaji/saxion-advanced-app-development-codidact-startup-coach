c5a4cf0fe801c55802d746c407ea99f6ed56d841
Nils Kimenai <493217@student.saxion.nl>
2023-12-12 09:20:02 +0100

    label styling

---
+++ frontend/src/components/labels/TextLabel.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <span
+    class="bg-secondary-subtle text-light-emphasis text- px-4 py-2 me-2 mt-2 rounded-pill"
+  >
+    <slot></slot>
+  </span>
+</template>
