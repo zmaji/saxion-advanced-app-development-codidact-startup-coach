239a839dc51c9bb8f18e34200e5bc567f4ecb1d7
Maurice <502241@student.saxion.nl>
2023-12-12 10:53:56 +0100

    Fixed typo's and wrongly deleted typings and imports

--- frontend/src/views/knowledgeBase/Index.vue
+++ frontend/src/views/knowledgeBase/Index.vue
@@ -1,36 +1,36 @@
 <script setup lang="ts">
 
-  import type { Category } from '@/typings/Content';
-  import type { Ref } from 'vue';
+import type { Category } from '@/typings/Category';
+import type { Ref } from 'vue';
 
-  import { onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
-  import {
-    ContentCategory,
-    CategoryBreadCrumb,
-    PageTitle,
-    SubTitle,
-    SecondaryTitle
-  } from '@/components';
-  import httpService from '@/plugins/http/httpService';
+import {
+  ContentCategory,
+  CategoryBreadCrumb,
+  PageTitle,
+  SubTitle,
+  SecondaryTitle
+} from '@/components';
+import httpService from '@/plugins/http/httpService';
 
-  const categories: Ref<Category[]> = ref<Category[]>([]);
+const categories: Ref<Category[]> = ref<Category[]>([]);
 
-  const fetchCategories = async () => {
-    try {
-      const response = await httpService.getRequest<Category[]>('/categories', false);
+const fetchCategories = async () => {
+  try {
+    const response = await httpService.getRequest<Category[]>('/categories', false);
 
-      if (response && response.data) {
-        categories.value = response.data;
-      }
-    } catch (e) {
-      console.error(e);
+    if (response && response.data) {
+      categories.value = response.data;
     }
+  } catch (e) {
+    console.error(e);
   }
+}
 
-  onMounted(() => {
-    fetchCategories();
-  });
+onMounted(() => {
+  fetchCategories();
+});
 </script>
 
 <template>
@@ -40,18 +40,13 @@
 
   <SecondaryTitle>Secondary header</SecondaryTitle>
 
-  <CategoryBreadCrumb/>
+  <CategoryBreadCrumb />
 
   <div class="accordion accordion-flush" id="categories-accordion">
     <div class="accordion-item" v-for="(category, key) in categories" :key="key">
-      <ContentCategory
-        :category="category"
-        :categoryKey="key"
-      />
+      <ContentCategory :category="category" :categoryKey="key" />
     </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
