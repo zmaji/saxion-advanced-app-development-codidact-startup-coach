cb901a9f45970bcf816bc1ca9833d941bc15ecaf
Maurice <502241@student.saxion.nl>
2023-12-19 16:07:24 +0100

    Getting all sets, questions and options linked to a section

--- backend/src/controllers/AnalysisSection.ts
+++ backend/src/controllers/AnalysisSection.ts
@@ -1,6 +1,9 @@
 import type { AnalysisSection } from '../typings/AnalysisSection';
 
 import analysisSectionModel from '../models/AnalysisSection';
+import questionSetsModel from '../models/QuestionSet';
+import questionModel from '../models/Question';
+import questionOptionModel from '../models/QuestionOption';
 
 const getAnalysisSections = async (): Promise<AnalysisSection[]> => {
   try {
@@ -13,14 +16,44 @@ const getAnalysisSections = async (): Promise<AnalysisSection[]> => {
 
 const getAnalysisSection = async (analysisSectionID: string): Promise<AnalysisSection | null> => {
   try {
-    const result = await analysisSectionModel.findOne({ analysisSectionID }, { _id: 0 });
+    const analysisSection = await analysisSectionModel.findOne({ analysisSectionID }, { _id: 0 });
 
-    return result || null;
+    if (!analysisSection) {
+      return null;
+    }
+
+    const questionSets = await questionSetsModel.find({ analysisSectionID }, { _id: 0 });
+
+    if (!questionSets || questionSets.length === 0) {
+      return analysisSection.toObject();
+    }
+
+    const questionSetsWithQuestions = await Promise.all(
+      questionSets.map(async (questionSet) => {
+        const questions = await questionModel.find({ questionSetID: questionSet.questionSetID }, { _id: 0 });
+
+        if (!questions || questions.length === 0) {
+          return questionSet.toObject();
+        }
+
+        const questionsWithOptions = await Promise.all(
+          questions.map(async (question) => {
+            const questionOptions = await questionOptionModel.find({ questionID: question.questionID }, { _id: 0 });
+            return { ...question.toObject(), questionOptions };
+          })
+        );
+
+        return { ...questionSet.toObject(), questions: questionsWithOptions };
+      })
+    );
+
+    return { ...analysisSection.toObject(), questionSets: questionSetsWithQuestions };
   } catch (error) {
     console.error('Something went wrong getting an analysis section:', error);
     throw error;
   }
 };
+
 
 const analysisSectionController = {
   getAnalysisSections,
