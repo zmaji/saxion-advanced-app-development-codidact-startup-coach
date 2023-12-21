7210a07c263cae022708ad87a73344851d6bb4ff
Saied Omar <470713@student.saxion.nl>
2023-12-21 13:39:53 +0100

    check of gebruikersbedrijfroadmap gelijk is aan de gevragde roadmap

--- backend/src/routes/Roadmap.ts
+++ backend/src/routes/Roadmap.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import roadmapController from '../controllers/Roadmap';
+import { Company } from '../typings/Company';
+import CompanyModel from '../models/Company';
 
 const router = Router();
 
@@ -26,7 +28,23 @@ router.get('/', async (req: Request, res: Response) => {
 
 router.get('/:roadmapID', async (req: Request, res: Response) => {
   try {
-    const result = await roadmapController.getRoadmap(req.params.roadmapID);
+    const requestedRoadmap: string = req.params.roadmapID;
+    const userCompanyID = req.user?.company;
+    const company: Company | null = await CompanyModel.findOne(userCompanyID);
+
+    if (!company) {
+      res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ error: 'Unable to find user company' });
+    }
+
+    if (company?.roadmap !== requestedRoadmap) {
+      res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ error: 'An error occurred getting company roadmap' });
+    }
+
+    const result = await roadmapController.getRoadmap(requestedRoadmap);
 
     if (result) {
       res
