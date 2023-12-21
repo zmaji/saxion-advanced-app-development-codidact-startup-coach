f64eb5e9ed8c99d7abda9edfedf5240df194d346
Saied Omar <470713@student.saxion.nl>
2023-12-21 13:57:19 +0100

    fout afhandeling

--- backend/src/routes/Roadmap.ts
+++ backend/src/routes/Roadmap.ts
@@ -28,20 +28,18 @@ router.get('/', async (req: Request, res: Response) => {
 
 router.get('/:roadmapID', async (req: Request, res: Response) => {
   try {
-    const requestedRoadmap: string = req.params.roadmapID;
+    const requestedRoadmap = req.params.roadmapID;
     const userCompanyID = req.user?.company;
     const company: Company | null = await CompanyModel.findOne(userCompanyID);
 
-    if (!company) {
-      res
-          .status(StatusCodes.NOT_FOUND)
-          .json({ error: 'Unable to find user company' });
-    }
+    if (!company || company.roadmap !== requestedRoadmap) {
+      const statusCode = !company ? StatusCodes.NOT_FOUND : StatusCodes.FORBIDDEN;
+      const errorMessage = !company ? 'Unable to find user company' :
+        'An error occurred, company roadmap and requested roadmap are not qual';
 
-    if (company?.roadmap !== requestedRoadmap) {
-      res
-          .status(StatusCodes.INTERNAL_SERVER_ERROR)
-          .json({ error: 'An error occurred getting company roadmap' });
+      return res
+          .status(statusCode)
+          .json({ error: errorMessage });
     }
 
     const result = await roadmapController.getRoadmap(requestedRoadmap);
@@ -55,10 +53,9 @@ router.get('/:roadmapID', async (req: Request, res: Response) => {
           .status(StatusCodes.NOT_FOUND)
           .json({ error: 'Unable to find requested roadmap' });
     }
-  } catch {
-    res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ error: 'An error occurred getting requested roadmap' });
+  } catch (error) {
+    console.error('Something went wrong getting requested:', error);
+    throw error;
   }
 });
 
