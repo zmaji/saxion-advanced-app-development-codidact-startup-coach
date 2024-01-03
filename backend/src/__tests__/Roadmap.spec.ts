4d03418d96d27dc57a0a4a5487a8e4fcbe96010c
Saied Omar <470713@student.saxion.nl>
2024-01-03 22:06:51 +0100

    Roadmap tests

--- backend/src/__tests__/Roadmap.spec.ts
+++ backend/src/__tests__/Roadmap.spec.ts
@@ -4,11 +4,52 @@ import { roadmapIndexData } from './mocks/data/Roadmap';
 import { app } from './config/setupFile';
 
 describe('GET /roadmaps/companyRoadmap', () => {
-  it('should return the roadmap of the user that is logged in', async () => {
+  it('should return the roadmap with modules and steps of the user that is logged in',
+      async () => {
+        const loginResponse = await request(app)
+            .post('/credentials')
+            .send({
+              userName: 'Zikria',
+              password: 'Zikria123',
+            });
+
+        const response = await request(app)
+            .get(`/roadmaps/companyRoadmap`)
+            .set('Authorization', `Bearer ${loginResponse.body.token}`);
+
+        expect(response.status).toBe(StatusCodes.OK);
+        expect(response.body).toEqual(roadmapIndexData[0]);
+      });
+
+  it('should return the roadmap with no modules of the user that is logged in'
+      , async () => {
+        const loginResponse = await request(app)
+            .post('/credentials')
+            .send({
+              userName: 'Maurice',
+              password: 'Maurice123',
+            });
+
+        const response = await request(app)
+            .get(`/roadmaps/companyRoadmap`)
+            .set('Authorization', `Bearer ${loginResponse.body.token}`);
+
+        expect(response.status).toBe(StatusCodes.OK);
+        expect(response.body).toEqual(roadmapIndexData[1]);
+      });
+
+  it('should handle a user who is not logged in', async () => {
+    const response = await request(app).get(`/roadmaps/companyRoadmap`);
+
+    expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
+    expect(response.body).toEqual({ error: 'Authentication required' });
+  });
+
+  it('should handle an invalid companyID', async () => {
     const loginResponse = await request(app)
         .post('/credentials')
         .send({
-          userName: 'Zikria',
+          userName: 'Zikria2',
           password: 'Zikria123',
         });
 
@@ -16,8 +57,24 @@ describe('GET /roadmaps/companyRoadmap', () => {
         .get(`/roadmaps/companyRoadmap`)
         .set('Authorization', `Bearer ${loginResponse.body.token}`);
 
-    expect(response.status).toBe(StatusCodes.OK);
-    expect(response.body).toEqual(roadmapIndexData[0]);
+    expect(response.status).toBe(StatusCodes.FORBIDDEN);
+    expect(response.body).toEqual({ error: 'User is not linked to a company or companyID is invalid' });
+  });
+
+  it('should handle an invalid roadmapID', async () => {
+    const loginResponse = await request(app)
+        .post('/credentials')
+        .send({
+          userName: 'Zikria3',
+          password: 'Zikria123',
+        });
+
+    const response = await request(app)
+        .get(`/roadmaps/companyRoadmap`)
+        .set('Authorization', `Bearer ${loginResponse.body.token}`);
+
+    expect(response.status).toBe(StatusCodes.FORBIDDEN);
+    expect(response.body).toEqual({ error: 'User is not linked to a company or companyID is invalid' });
   });
 });
 
--- backend/src/routes/Roadmap.ts
+++ backend/src/routes/Roadmap.ts
@@ -34,11 +34,19 @@ router.get('/companyRoadmap', isLoggedIn, async (req: Request, res: Response) =>
         res
             .status(StatusCodes.OK)
             .json(result);
+      } else if (result === null) {
+        res
+            .status(StatusCodes.FORBIDDEN)
+            .json({ error: 'User is not linked to a company or companyID is invalid' });
       } else {
         res
             .status(StatusCodes.NOT_FOUND)
             .json({ error: 'Unable to find requested roadmap' });
       }
+    } else {
+      res
+          .status(StatusCodes.UNAUTHORIZED)
+          .json({ error: 'User is not logged in' });
     }
   } catch (error) {
     console.error('Something went wrong getting requested:', error);
