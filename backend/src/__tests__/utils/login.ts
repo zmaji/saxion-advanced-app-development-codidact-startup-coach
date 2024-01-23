bdbed7d4e4f25393b34e7a850a8ee782ad23e104
Nils Kimenai <493217@student.saxion.nl>
2024-01-23 13:58:09 +0100

    jsdoc for util and jest config

--- backend/src/__tests__/utils/login.ts
+++ backend/src/__tests__/utils/login.ts
@@ -1,23 +1,31 @@
 import request from 'supertest';
 import { Express } from 'express';
 
+/**
+ * Log the user in and return a token to use within the test
+ * @param {Express} app - The current instance of the app within the test
+ * @param {string} userName - The username of the user which needs to log in
+ * @param {string} password - The password of the user to login
+ * @return {Promise<string>} token - The response of the login of the user, in this case the token
+ */
 export function loginAsUser(
-    app: Express, 
-    userName: string, 
-    password: string
+    app: Express,
+    userName: string,
+    password: string,
 ): Promise<string> {
-    return new Promise<string>(async (resolve, reject) => {
-        try {
-            const response = await request(app)
-                .post('/credentials')
-                .send({
-                    userName: userName,
-                    password: password,
-                });
+  // eslint-disable-next-line no-async-promise-executor
+  return new Promise<string>(async (resolve, reject) => {
+    try {
+      const response = await request(app)
+          .post('/credentials')
+          .send({
+            userName: userName,
+            password: password,
+          });
 
-            resolve(response.body.token);
-        } catch (error) {
-            reject(error);
-        }
-    });
+      resolve(response.body.token);
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
